af7fbfb131b54e765a8ea64d3ea20bb01deb6dab
me <119694935+username66653@users.noreply.github.com>
2023-06-09 20:21:58 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import subprocess
+import os
+print("installing")
+print("installing the system")
+os.chdir(r"C:\ ")
+print("the system will be installed in the C:\systemos directory")
+os.mkdir("systemos")
+os.chdir("C:\systemos")
+print("systemos made install system")
+print("installing")
+os.mkdir("C:\systemos\system")
+os.chdir("C:\systemos\system")
+subprocess.Popen("curl https://raw.githubusercontent.com/username66653/wget/main/systemmain.py -o systemmain.py")
+print("installed")
+print("configuring")
+os.chdir("C:\systemos")
+os.mkdir("home")
+print("installed")
